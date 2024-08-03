666ea31cc0bde64f663a3ae0a9a2c1e86da825cb
Sergey Mishnev <s.mishnev@forsit.de>
2024-08-03 15:50:12 +0300

    feat: mutate original variable using pointer

--- pointers.go
+++ pointers.go
@@ -10,11 +10,12 @@ func pointer() {
 
 	fmt.Println("Age: ", *agePointer)
 
-	adultYears := getAdultYears(agePointer)
+	getAdultYears(agePointer)
 
-	fmt.Println("Adult years: ", adultYears)
+	fmt.Println("Adult years: ", age)
 }
 
-func getAdultYears(age *int) int {
-	return *age - 18
+func getAdultYears(age *int) {
+	*age = *age - 18
+	// return *age - 18
 }
