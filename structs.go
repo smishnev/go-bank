e8d46236b8239f68cd483a90311e4ea006bf5b7b
Sergey Mishnev <s.mishnev@forsit.de>
2024-08-04 12:35:28 +0300

    feat: add structs

---
+++ structs.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type user struct {
+	firstName string
+	lastName  string
+	birthDate string
+	createdAt time.Time
+}
+
+func structs() {
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthDate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	var appUser user
+
+	appUser = user{
+		firstName: userFirstName,
+		lastName:  userLastName,
+		birthDate: userBirthDate,
+		createdAt: time.Now(),
+	}
+
+	outputUserDetails(appUser)
+}
+
+func outputUserDetails(appUser user) {
+	fmt.Println(appUser.firstName, appUser.lastName, appUser.birthDate, appUser.createdAt)
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+	fmt.Scan(&value)
+	return value
+}
