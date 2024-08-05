f398a059fda9884b69519dc4308ac4c6327a1813
Sergey Mishnev <s.mishnev@forsit.de>
2024-08-05 09:05:17 +0300

    feat: create note package

--- main.go
+++ main.go
@@ -2,82 +2,124 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
-	"example.com/main/fileops"
-	"github.com/Pallinder/go-randomdata"
+	"bufio"
+
+	"example.com/main/note"
+	// "example.com/main/fileops"
+	// "github.com/Pallinder/go-randomdata"
 )
 
 const accountBalanceFile = "balance.txt"
 
 func main() {
-	var accountBalance, err = fileops.GetFloatFromFile(accountBalanceFile, 1000)
+	// var accountBalance, err = fileops.GetFloatFromFile(accountBalanceFile, 1000)
 
-	pointer()
+	// pointer()
 
-	structs()
+	// structs()
 
-	fmt.Println("--------------------")
+	// fmt.Println("--------------------")
+
+	// if err != nil {
+	// 	fmt.Println("Error")
+	// 	fmt.Println(err)
+	// 	fmt.Println("--------------------")
+
+	// 	// panic("Can't continue, Sorry")
+	// }
+
+	// fmt.Println("Welcome to Go Bank")
+	// fmt.Println("Reach us 24/7", randomdata.PhoneNumber())
+
+	// for {
+	// 	presentOptions()
+
+	// 	var choice int
+	// 	fmt.Print("Your choice: ")
+	// 	fmt.Scan(&choice)
+
+	// 	switch choice {
+	// 	case 1:
+	// 		fmt.Println("Your balance is: ", accountBalance)
+	// 	case 2:
+	// 		fmt.Print("How much do you want to deposit?: ")
+	// 		var depositAmount float64
+	// 		fmt.Scan(&depositAmount)
+
+	// 		if depositAmount <= 0 {
+	// 			fmt.Println("Invalid amount. Must be greater than 0")
+
+	// 			continue
+	// 		}
+
+	// 		accountBalance += depositAmount
+	// 		fmt.Println("Your balance is: ", accountBalance)
+	// 		fileops.WriteFloatToFile(accountBalance, accountBalanceFile)
+	// 	case 3:
+	// 		fmt.Print("How much do you want to withdraw?: ")
+	// 		var withdrawAmount float64
+	// 		fmt.Scan(&withdrawAmount)
+
+	// 		if withdrawAmount <= 0 {
+	// 			fmt.Println("Invalid amount. Must be greater than 0")
+	// 			continue
+	// 		}
+
+	// 		if withdrawAmount > accountBalance {
+	// 			fmt.Println("Invalid amount. You can't withdraw more than your balance")
+	// 			continue
+	// 		}
+
+	// 		accountBalance -= withdrawAmount
+	// 		fmt.Println("Your balance is: ", accountBalance)
+	// 		fileops.WriteFloatToFile(accountBalance, accountBalanceFile)
+	// 	default:
+	// 		fmt.Println("Bye!")
+	// 		fmt.Println("Thank you for using our Bank")
+
+	// 		return
+	// 	}
+
+	// }
+
+	title, content := getNoteData()
+
+	userNote, err := note.New(title, content)
 
 	if err != nil {
-		fmt.Println("Error")
 		fmt.Println(err)
-		fmt.Println("--------------------")
-
-		// panic("Can't continue, Sorry")
+		return
 	}
 
-	fmt.Println("Welcome to Go Bank")
-	fmt.Println("Reach us 24/7", randomdata.PhoneNumber())
+	userNote.Display()
 
-	for {
-		presentOptions()
+}
 
-		var choice int
-		fmt.Print("Your choice: ")
-		fmt.Scan(&choice)
+func getNoteData() (string, string) {
+	title := getUserInput("Note title: ")
 
-		switch choice {
-		case 1:
-			fmt.Println("Your balance is: ", accountBalance)
-		case 2:
-			fmt.Print("How much do you want to deposit?: ")
-			var depositAmount float64
-			fmt.Scan(&depositAmount)
+	content := getUserInput("Note content: ")
 
-			if depositAmount <= 0 {
-				fmt.Println("Invalid amount. Must be greater than 0")
+	return title, content
 
-				continue
-			}
+}
 
-			accountBalance += depositAmount
-			fmt.Println("Your balance is: ", accountBalance)
-			fileops.WriteFloatToFile(accountBalance, accountBalanceFile)
-		case 3:
-			fmt.Print("How much do you want to withdraw?: ")
-			var withdrawAmount float64
-			fmt.Scan(&withdrawAmount)
+func getUserInput(promptText string) string {
+	fmt.Printf("%v ", promptText)
 
-			if withdrawAmount <= 0 {
-				fmt.Println("Invalid amount. Must be greater than 0")
-				continue
-			}
+	reader := bufio.NewReader(os.Stdin)
 
-			if withdrawAmount > accountBalance {
-				fmt.Println("Invalid amount. You can't withdraw more than your balance")
-				continue
-			}
+	text, err := reader.ReadString('\n')
 
-			accountBalance -= withdrawAmount
-			fmt.Println("Your balance is: ", accountBalance)
-			fileops.WriteFloatToFile(accountBalance, accountBalanceFile)
-		default:
-			fmt.Println("Bye!")
-			fmt.Println("Thank you for using our Bank")
-
-			return
-		}
-
+	if err != nil {
+		return ""
 	}
 
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
+
+	return text
 }
