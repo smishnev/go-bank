10ac7b52cd6f59bab25e6ba649b45eb93723198c
Sergey Mishnev <s.mishnev@forsit.de>
2024-08-22 09:06:55 +0300

    feat: add recursion

--- main.go
+++ main.go
@@ -3,36 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	numbers := []int{1, 2, 3}
-	doubleFunc := createTransformerFunction(2)
-	tripleFunc := createTransformerFunction(3)
+	fact := factorial(5)
 
-	transformed := transformNumbers(&numbers, func(number int) int {
-		return number * 2
-	})
-
-	fmt.Println(transformed)
-
-	double := transformNumbers(&numbers, doubleFunc)
-	triple := transformNumbers(&numbers, tripleFunc)
-
-	fmt.Println(double)
-	fmt.Println(triple)
+	fmt.Println(fact)
 
 }
 
-func transformNumbers(numbers *[]int, transform func(int) int) []int {
-	dNumbers := []int{}
-
-	for _, val := range *numbers {
-		dNumbers = append(dNumbers, transform(val))
+func factorial(number int) int {
+	if number == 0 {
+		return 1
 	}
 
-	return dNumbers
-}
+	return number * factorial(number-1)
 
-func createTransformerFunction(factor int) func(int) int {
-	return func(number int) int {
-		return number * factor
-	}
 }
