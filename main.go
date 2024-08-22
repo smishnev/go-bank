435c05d7ac72436abf1728f4ecf2b3e90036b59b
Sergey Mishnev <s.mishnev@forsit.de>
2024-08-22 09:19:19 +0300

    feat: split array like number

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
 	sum := sumup(1, 15, 58)
 
+	anotherSum := sumup(numbers...)
+
+	fmt.Println(anotherSum)
 	fmt.Println(sum)
 }
 
